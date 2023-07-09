11e20db2100ce5714c2e474dc6d266c3bafcbc6c
Marius <mariusygonia@gmail.com>
2023-07-09 01:49:49 -0400

    Expanded player data

--- src/shared/constants/PlayerData.ts
+++ src/shared/constants/PlayerData.ts
@@ -1,9 +1,23 @@
 import { PlayerData } from "../types/PlayerData";
+import { Achievements } from "./Achievements";
 
 export const DEFAULT_PLAYER_DATA: PlayerData = {
 	cash: 100,
 	gems: 100,
+	progress: {
+		["city"]: "Toronto",
+		["restaurant"]: "Lemonade Stand",
+		["foodStationLevels"]: [0],
+	},
 	totalPlaytime: 0,
+	customersServed: 0,
+	cashEarned: 0,
+	cashSpent: 0,
+	gemsEarned: 0,
+	gemsSpent: 0,
+	chestsOpened: 0,
+	eventBadges: {},
+	achievements: Achievements,
 	settings: {
 		music: true,
 		sound_effects: true,
--- src/shared/types/PlayerData.ts
+++ src/shared/types/PlayerData.ts
@@ -1,7 +1,26 @@
+enum badgeLevels {
+	Bronze,
+	Silver,
+	Gold,
+}
+
 export interface PlayerData {
 	cash: number;
 	gems: number;
+	progress: {
+		["city"]: string;
+		["restaurant"]: string;
+		["foodStationLevels"]: Array<number>;
+	};
 	totalPlaytime: number;
+	customersServed: number;
+	cashEarned: number;
+	cashSpent: number;
+	gemsEarned: number;
+	gemsSpent: number;
+	chestsOpened: number;
+	achievements: { [id: string]: boolean };
+	eventBadges: { [eventName: string]: { dateEarned: number; levelAchieved: badgeLevels } };
 	settings: Settings;
 }
 
